62a8f7d4d6f78d8bfbcd1b908c622bba3499a7a6
vishal-kumar4 <vishal8143667512@gmail.com>
2023-03-07 14:44:48 +0530

    edited one navbar li in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
         <div>
             <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand mx-3" to="/"><FaBuffer/></Link>
+                    <Link className="navbar-brand mx-3" to="/"><FaBuffer/><span className='mx-1'>NewsBee</span></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
